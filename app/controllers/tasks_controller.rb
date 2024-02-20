415c4151f5dd8c81c26516811fac0b728654d4bb
Martin <155843922+cap89@users.noreply.github.com>
2024-02-20 16:40:15 +0100

    update full working to-do-list

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,46 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to task_path(@task)
+    else
+      render :new
+    end
+  end
+
+  def update
+    set_task
+    @task.update(task_params)
+    @task.save
+    redirect_to task_path(@task.id)
+  end
+
+  def destroy
+    set_task
+    @task.destroy
+    redirect_to index_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,17 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  get "/index", to: "tasks#index"
+  get "/index/:id", to: "tasks#show", as: :task
+
+  get "/tasks/new", to: "tasks#new"   # gets the form
+  post "/tasks", to: "tasks#create" # post (creating id)
+
+  # Update (two HTTP Requests)
+  get "/index/:id/edit", to: "tasks#edit", as: :edit # gets filled in form
+  patch "/index/:id", to: "tasks#update" #saves it
+
+  # Delete
+  delete "/index/:id", to: "tasks#destroy", as: :destroy
 end
